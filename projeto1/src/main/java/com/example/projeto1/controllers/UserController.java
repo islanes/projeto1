f4c7113c4331fb027290c6afec1b38e2ca3ebf8d
Islane <islaness.yes@gmail.com>
2019-08-04 13:29:01 -0300

    Adicionado um service para regras de negócios

--- projeto1/src/main/java/com/example/projeto1/controllers/UserController.java
+++ projeto1/src/main/java/com/example/projeto1/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.example.projeto1.controllers;
 
 import com.example.projeto1.repository.UserRepository;
+import com.example.projeto1.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,16 +14,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class UserController {
 
-    private UserRepository userRepository;
-
-    public UserController(UserRepository userRepository){
-     this.userRepository = userRepository;
-    }
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(value = "/user",method = RequestMethod.GET)
     //@ResponseBody
     public String getUser(Model model){
-        model.addAttribute("usersList", this.userRepository.findAll());
+        model.addAttribute("usersList", this.userService.findAll());
 
         return "user";//retornando um template
     }
